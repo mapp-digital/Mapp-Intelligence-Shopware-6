1764d7afd712a05c725dbd9a9f6ade131f4a7756
Sascha Stieglitz# <sascha.stieglitz@web.de>
2020-06-04 11:00:45 +0200

    Getting rid of dal dealer

--- src/Subscriber/HeaderSubscriber.php
+++ src/Subscriber/HeaderSubscriber.php
@@ -7,7 +7,6 @@ use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Shopware\Storefront\Pagelet\Header\HeaderPageletLoadedEvent;
 use Shopware\Core\Framework\Struct\ArrayEntity;
-use Mapp\MappIntelligence\Services\DalDealer;
 
 class HeaderSubscriber implements EventSubscriberInterface
 {
@@ -16,18 +15,11 @@ class HeaderSubscriber implements EventSubscriberInterface
      */
     private $systemConfigService;
 
-    /**
-     * @var DalDealer
-     */
-    private $dalDealer;
-
     public function __construct(
-        SystemConfigService $systemConfigService,
-        DalDealer $dalDealer
+        SystemConfigService $systemConfigService
     )
     {
         $this->systemConfigService = $systemConfigService;
-        $this->dalDealer = $dalDealer;
     }
 
     public static function getSubscribedEvents()
@@ -43,11 +35,7 @@ class HeaderSubscriber implements EventSubscriberInterface
      */
     public function onHeaderLoaded(HeaderPageletLoadedEvent $event)
     {
-        $route = $event->getRequest()->attributes->get('_route');
         $salesChannelId = $event->getSalesChannelContext()->getSalesChannel()->getId();
-//        $systemConfig = $this->systemConfigService->getDomain('MappIntelligence', $salesChannelId);
-
-
         $config = $this->systemConfigService->get('MappIntelligence.config',  $salesChannelId);
         if(!isset($config['tiDomain'])) {
             $config['tiDomain'] = 'responder.wt-safetag.com';
@@ -55,10 +43,5 @@ class HeaderSubscriber implements EventSubscriberInterface
 
         $page = $event->getPagelet();
         $page->addExtension('tiLoader', new ArrayEntity($config));
-
-        $data = array(
-            '_ti' => $this->dalDealer->getData($event)
-        );
-        $page->addExtension('mappIntelligence', new ArrayEntity($data));
     }
 }
