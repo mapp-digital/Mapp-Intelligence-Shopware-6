090c613a5106f5c0305bd1015834f0b1887b79a6
Sascha Stieglitz <sascha.stieglitz@web.de>
2020-08-14 21:10:22 +0200

    TRACKING-366 TRACKING-258 test consent settings

--- src/Resources/app/storefront/test/e2e/cypress/integration/09_consent.spec.js
+++ src/Resources/app/storefront/test/e2e/cypress/integration/09_consent.spec.js
@@ -1,22 +1,69 @@
 // / <reference types="Cypress" />
 
-describe('MappIntelligencePluginTests: Homepage', () => {
-
-    beforeEach(() => {
-        cy.visit('/')
-    });
-
+describe('MappIntelligencePluginTests: Cookie Consent Manager', () => {
     it('no consent -> no tiLoader', () => {
+        cy.visit('/');
         cy.window()
             .then((win) => {
-                data = win._ti;
-            })
-            .then(() => {
-                expect(data.pageName).to.equal('localhost:8000/');
-                expect(data.pageNumber).to.equal('1');
-                expect(data.pageTitle).to.equal('Catalogue #1');
-                expect(data.contentCategory).to.equal('Home');
-                expect(data.contentSubcategory).to.equal('Product Overview');
+                expect(win.wts).to.not.exist;
             });
+    });
+
+    it('track request after consent', () => {
+         cy.visit('/');
+        cy.window()
+            .then((win) => {
+                expect(win.wts).to.not.exist;
+            });
+        cy.get('.js-cookie-configuration-button button').click({force:true});
+        cy.contains('Mapp Intelligence').click();
+        cy.contains('Save').click();
+        cy.window()
+            .then((win) => {
+                expect(win.wts).to.exist;
+            });
+    });
+
+    it('Deactivate Consent Manager via Plugin Settings', () => {
+        cy.server();
+        cy.route({
+            url: '/api/v*/_action/system-config/batch',
+            method: 'post'
+        }).as('saveData');
+
+        // cy.loginViaApi()
+        //     .then(() => {
+        //         cy.visit('/admin#/sw/plugin/settings/MappIntelligence');
+        //     });
+        // cy.contains('List Mapp Intelligence in Shopware 6 Cookie Consent Manager').click();
+        // cy.get('.sw-field--checkbox__content input[type=checkbox]').check();
+        // cy.get('.sw-plugin-config__save-action').click();
+        // cy.wait('@saveData');
+
+        cy.visit('/');
+        cy.window()
+            .then((win) => {
+                expect(win.wts).to.not.exist;
+            });
+        cy.get('.js-cookie-configuration-button button').click({force:true});
+        cy.contains('Cookie Preferences').should('be.visible');
+        cy.contains('Mapp Intelligence').should('be.visible');
+
+        cy.loginViaApi()
+            .then(() => {
+                cy.visit('/admin#/sw/plugin/settings/MappIntelligence');
+            });
+        cy.contains('List Mapp Intelligence in Shopware 6 Cookie Consent Manager').click();
+        cy.get('.sw-field--checkbox__content input[type=checkbox]').uncheck();
+        cy.get('.sw-plugin-config__save-action').click();
+        cy.wait('@saveData');
+        cy.visit('/');
+        cy.window()
+            .then((win) => {
+                expect(win.wts).to.exist;
+            });
+        cy.get('.js-cookie-configuration-button button').click({force:true});
+        cy.contains('Cookie Preferences').should('be.visible');
+        cy.contains('Mapp Intelligence').should('not.be.visible');
     });
 });
